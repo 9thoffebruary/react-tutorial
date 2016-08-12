6312996fac41a7bc0156ef62fbc4b1999fe6307a
9thoffebruary <apinya.sirikunchorn@gmail.com>
2016-08-12 13:13:28 +0700

    Install webpack babel react to project

---
+++ src/client/app/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+  render () {
+    return(
+      <div>
+        <p> Hello Saille! - My first react setup</p>
+        <h3>Testing</h3>
+      </div>
+    )
+  }
+}
+
+render(<App/>, document.getElementById('app'));
